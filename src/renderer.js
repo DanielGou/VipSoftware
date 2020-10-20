2da444bd15b50490e8141e1ef2c630eecf63d825
DanielGou <danielpraiadorosa@gmail.com>
2020-10-20 15:52:19 -0300

    1.0.3

--- src/renderer.js
+++ src/renderer.js
@@ -110,26 +110,11 @@ ipcRenderer.on('app_version', (event, arg) => {
     version.innerText += ' ' + arg.version;
 });
 
-// Notificação de atualização
-const notification = document.getElementById('notification');
-const message = document.getElementById('message');
-const restartButton = document.getElementById('restart-button');
-ipcRenderer.on('update_available', () => {
-  ipcRenderer.removeAllListeners('update_available');
-  message.innerText = 'A new update is available. Downloading now...';
-  notification.classList.remove('hidden');
-});
-ipcRenderer.on('update_downloaded', () => {
-  ipcRenderer.removeAllListeners('update_downloaded');
-  message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
-  restartButton.classList.remove('hidden');
-  notification.classList.remove('hidden');
-});
-
-// Restart do app
-function closeNotification() {
-    notification.classList.add('hidden');
-}
-function restartApp() {
-    ipcRenderer.send('restart_app');
-}
+// Listen for messages
+const {ipcRenderer} = require('electron');
+ipcRenderer.on('message', function(event, text) {
+  var container = document.getElementById('messages');
+  var message = document.createElement('div');
+  message.innerHTML = text;
+  container.appendChild(message);
+})
